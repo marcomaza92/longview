b0c5380ce43e3009dd85f74d42e68332d529f15d
marcomaza92 <marcoagustinmaza@gmail.com>
2021-02-27 17:14:10 -0300

    feat(UI): add Material UI

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,16 +1,20 @@
+import { CircularProgress } from '@material-ui/core';
 import { useQuery, useQueryClient } from 'react-query';
 import { displayMovies } from '../../api/display';
 import styles from './Controls.module.scss';
 
 function Controls() {
-  const queryClient = useQueryClient();
   const query = useQuery('movies', displayMovies);
   console.log(query);
   return (
     <div className={styles.main}>
-      {query.data?.categories[0].videos.map((item, index) => (
-        <p key={index}>{item.description}</p>
-      ))}
+      {query.isLoading ? (
+        <CircularProgress />
+      ) : (
+        query.data?.categories[0].videos.map((item, index) => (
+          <p key={index}>{item.description}</p>
+        ))
+      )}
     </div>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { QueryClientProvider } from 'react-query';
 import { queryClient } from '../../api/common';
 import Controls from '../../components/Controls';
+import Display from '../../components/Display';
 import { ThemeContext } from '../../context/Theme';
 import styles from './Home.module.scss';
 
@@ -11,6 +12,7 @@ const Home = () => {
     <div className={`${styles.main} ${styles[theme]}`}>
       <h1>Home page</h1>
       <QueryClientProvider client={queryClient}>
+        <Display />
         <Controls />
       </QueryClientProvider>
     </div>
