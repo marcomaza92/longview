1b13af824a5a580fc57119f205f57cc6e6a3be9d
marcomaza92 <marcoagustinmaza@gmail.com>
2021-03-04 02:52:07 -0300

    chore(app): remove unused code and update types

--- src/api/display/index.ts
+++ src/api/display/index.ts
@@ -1,6 +1,6 @@
 import { BaseUrl, DataUrl } from '../common';
 
-export const displayMovies = async (): Promise<Record<string, any>> => {
+export const displayMovies = async (): Promise<Record<string, unknown>> => {
   try {
     const response = await fetch(`${BaseUrl}${DataUrl}`, {
       method: 'GET',
